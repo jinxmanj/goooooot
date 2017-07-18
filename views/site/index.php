4fabc703b7153405e9b6ec818b867b33ec49bd05
manjinxin <manjinxin@baidu.com>
2017-07-18 18:37:28 +0800

    stackable

--- views/site/index.php
+++ views/site/index.php
@@ -16,9 +16,7 @@ $this->registerJs($script, View::POS_END);
     La vie est courte, commencez par le dessert !
 </h2>
 <div class="ui horizontal divider inverted">Or</div>
-<div class="ui grid special cards">
-    <div class="four wide column">
-    </div>
+<div class="ui grid special cards doubling stackable">
     <div class="four wide column">
         <div class="ui card">
             <div class="blurring dimmable image">
@@ -66,6 +64,50 @@ $this->registerJs($script, View::POS_END);
         </div>
     </div>
     <div class="four wide column">
+        <div class="ui card">
+            <div class="blurring dimmable image">
+                <div class="ui dimmer">
+                    <div class="content">
+                        <div class="center">
+                            <div class="ui inverted button">Coming Soon</div>
+                        </div>
+                    </div>
+                </div>
+                <img src="/images/avatar/large/elliot.jpg">
+            </div>
+            <div class="content">
+                <a class="header">Team Fu</a>
+                <div class="meta">
+                    <span class="date">Create in Sep 2014</span>
+                </div>
+            </div>
+            <div class="extra content">
+                <a><i class="users icon"></i> 2 Members </a>
+            </div>
+        </div>
+    </div>
+    <div class="four wide column">
+        <div class="ui card">
+            <div class="blurring dimmable image">
+                <div class="ui inverted dimmer">
+                    <div class="content">
+                        <div class="center">
+                            <div class="ui primary button">Coming Soon</div>
+                        </div>
+                    </div>
+                </div>
+                <img src="/images/avatar/large/jenny.jpg">
+            </div>
+            <div class="content">
+                <a class="header">Team Hess</a>
+                <div class="meta">
+                    <span class="date">Create in Aug 2014</span>
+                </div>
+            </div>
+            <div class="extra content">
+                <a><i class="users icon"></i> 2 Members </a>
+            </div>
+        </div>
     </div>
 </div>
 
